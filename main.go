d15f57351a50faad02fc9fdf0de1684e30d9c6ed
fjohnson <fjohnson@peoplenetonline.com>
2017-09-06 20:23:42 -0500

    Trigger a drone build

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
 )
- 
+  
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
