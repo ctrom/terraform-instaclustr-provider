ad80e548c6e81912c81745c26425506173da29d3
fjohnson <fjohnson@peoplenetonline.com>
2017-11-08 15:19:47 -0600

    trigger ci

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
 )
-  
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return Provider() 
 		},
 	})
 }
